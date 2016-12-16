4cf228114bcad6d34d27d398746d5eec9298a377
Matt Porter <mporter@konsulko.com>
2016-12-16 15:22:52 -0500

    Replace POI app with Mixer app, reuse Settings icon as we don't yet have a proper mixer icon
    Signed-off-by: Matt Porter <mporter@konsulko.com>

--- HomeScreen/src2/applicationmodel.cpp
+++ HomeScreen/src2/applicationmodel.cpp
@@ -49,7 +49,7 @@ ApplicationModel::Private::Private(ApplicationModel *parent)
         data.append(AppInfo(QStringLiteral("Connectivity"), QStringLiteral("CONNECTIVITY"), QStringLiteral("connectivity@0.1")));
         data.append(AppInfo(QStringLiteral("Dashboard"), QStringLiteral("DASHBOARD"), QStringLiteral("dashboard@0.1")));
         data.append(AppInfo(QStringLiteral("Settings"), QStringLiteral("SETTINGS"), QStringLiteral("settings@0.1")));
-        data.append(AppInfo(QStringLiteral("POI"), QStringLiteral("POINT OF\nINTEREST"), QStringLiteral("poi@0.1")));
+        data.append(AppInfo(QStringLiteral("Mixer"), QStringLiteral("MIXER"), QStringLiteral("mixer@0.1")));
     }
 }
 
